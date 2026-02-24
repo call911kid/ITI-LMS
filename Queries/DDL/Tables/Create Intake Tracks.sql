32ad9c38dd693e97753232b6c6ffa50f2ebbef9e
Mustafa Hassan <mustafawork911@gmail.com>
2026-02-24 17:36:22 +0200

    Department and IntakeTrack constraints to match the previous alter query

--- Queries/DDL/Tables/Create Intake Tracks.sql
+++ Queries/DDL/Tables/Create Intake Tracks.sql
@@ -2,8 +2,10 @@ CREATE TABLE [IntakeTrack](
 	[IntakeTrackId] INT IDENTITY(1,1),
 	[IntakeId] INT NOT NULL,
 	[TrackId] INT NOT NULL,
+	[BranchId] INT NOT NULL,
 
 	CONSTRAINT PK_IntakeTrack PRIMARY KEY([IntakeTrackId]),
 	CONSTRAINT FK_IntakeTrack_Intake FOREIGN KEY([IntakeId]) REFERENCES [Intake]([IntakeId]),
-	CONSTRAINT FK_IntakeTrack_Track FOREIGN KEY([TrackId]) REFERENCES [Track]([TrackId])
+	CONSTRAINT FK_IntakeTrack_Track FOREIGN KEY([TrackId]) REFERENCES [Track]([TrackId]),
+	CONSTRAINT FK_IntakeTrack_Branch FOREIGN KEY([BranchId]) REFERENCES [Branch]([BranchId])
 )
