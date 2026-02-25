d3aa98128b7e1338142adccc4312079b2be6b214
amr sobhy <am2873328@gmail.com>
2026-02-25 16:10:37 +0200

    Create InstructorGetExamQuesions.sql

---
+++ Queries/Functions/InstructorGetExamQuesions.sql
@@ -0,0 +1,29 @@
+CREATE FUNCTION FN_GetExamQuesions
+(
+    @ExamId INT
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT 
+        eq.ExamQuestionId,
+        q.QuestionId,
+        q.QuestionType,
+        q.QuestionBody,
+        CASE 
+            WHEN q.QuestionType = 'MCQ' THEN 
+                (SELECT STRING_AGG(ch.ChoiceText, ', ')
+                 FROM Choice ch
+                 WHERE ch.QuestionId = q.QuestionId)
+            ELSE NULL
+        END AS MCQOptions,
+        q.CorrectAnswer,
+        eq.Marks
+    FROM ExamQuestion eq
+    JOIN Exam e ON eq.ExamId = e.ExamId
+    JOIN Class cl ON e.CourseId = cl.CourseId
+    JOIN Question q ON eq.QuestionId = q.QuestionId
+    WHERE e.ExamId = @ExamId
+      AND cl.InstructorId = dbo.GetCurrentUserID()
+);
