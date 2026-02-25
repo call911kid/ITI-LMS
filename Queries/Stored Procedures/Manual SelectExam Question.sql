7a721a37249a8e9e2cd94505f92e42461eb4d807
amr sobhy <am2873328@gmail.com>
2026-02-25 14:45:42 +0200

    Create Manual SelectExam Question.sql

---
+++ Queries/Stored Procedures/Manual SelectExam Question.sql
@@ -0,0 +1,26 @@
+CREATE PROCEDURE USP_ManualSelectExamQuestion
+    @ExamId INT,
+    @QuestionId INT,
+    @Marks DECIMAL(5,2) 
+AS
+BEGIN
+    DECLARE @InstructorId INT = dbo.GetCurrentUserID();
+
+    IF EXISTS (
+        SELECT 1
+        FROM Exams e
+        JOIN Courses c ON e.CourseId = c.CourseId
+        JOIN Questions q ON q.CourseId = c.CourseId
+        WHERE e.ExamId = @ExamId
+          AND q.QuestionId = @QuestionId
+          AND c.InstructorId = @InstructorId
+    )
+    BEGIN
+        INSERT INTO ExamQuestions (ExamId, QuestionId, Marks)
+        VALUES (@ExamId, @QuestionId, @Marks);
+    END
+    ELSE
+    BEGIN
+        RAISERROR('Instructor not allowed to add this question to the exam.', 16, 1);
+    END
+END;
