a338897edbb9d4dbb992ee1836c0bd0104f6aa6d
Reem Abdelkader <96788888+ReemAbdelkader@users.noreply.github.com>
2026-02-25 03:16:12 +0200

    Intake Trake Full Info

---
+++ Queries/Views/Intake Trake Info.sql
@@ -0,0 +1,25 @@
+--Creating Veiw IntakeTrake
+
+CREATE VIEW VW_IntakeTrackFullInfo AS
+SELECT
+B.[BranchId],
+B.[BranchName],
+I.[IntakeName],
+T.[TrackName]
+FROM [dbo].[Branch] B
+LEFT JOIN [dbo].[IntakeTrack] IT
+ON
+IT.BranchId = B.BranchId
+LEFT JOIN [dbo].[Intake] I
+ON
+I.IntakeId = IT.IntakeId
+LEFT JOIN [dbo].[Track] T
+ON
+T.TrackId = IT.TrackId
+
+
+
+
+
+
+
