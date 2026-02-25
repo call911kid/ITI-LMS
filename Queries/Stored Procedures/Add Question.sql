8db277214a75afd371e1227f8159ffaee40277cf
amr sobhy <am2873328@gmail.com>
2026-02-25 14:31:52 +0200

    Update Add Question.sql

--- Queries/Stored Procedures/Add Question.sql
+++ Queries/Stored Procedures/Add Question.sql
@@ -1,11 +1,12 @@
 
 CREATE PROCEDURE USP_AddQuestion
-    @InstructorId INT,
     @CourseId INT,
     @QuestionText NVARCHAR(MAX),
     @QuestionType NVARCHAR(10) -- 'MCQ', 'TF', 'Text'
 AS
 BEGIN
+    DECLARE @InstructorId INT = dbo.GetCurrentUserID();
+
     IF EXISTS (SELECT 1 FROM Courses WHERE CourseId = @CourseId AND InstructorId = @InstructorId)
     BEGIN
         INSERT INTO Questions (CourseId, QuestionText, QuestionType, CreatedBy)
@@ -17,4 +18,4 @@ BEGIN
     BEGIN
         RAISERROR('Instructor not allowed to add question to this course.', 16, 1);
     END
-END
+END;
