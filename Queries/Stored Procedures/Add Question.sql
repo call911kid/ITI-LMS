20008b8ab2b9f8a510a2e5f0f0a1ca7a0dec41c0
amr sobhy <am2873328@gmail.com>
2026-02-25 14:22:35 +0200

    Create Add Question.sql

---
+++ Queries/Stored Procedures/Add Question.sql
@@ -0,0 +1,20 @@
+
+CREATE PROCEDURE USP_AddQuestion
+    @InstructorId INT,
+    @CourseId INT,
+    @QuestionText NVARCHAR(MAX),
+    @QuestionType NVARCHAR(10) -- 'MCQ', 'TF', 'Text'
+AS
+BEGIN
+    IF EXISTS (SELECT 1 FROM Courses WHERE CourseId = @CourseId AND InstructorId = @InstructorId)
+    BEGIN
+        INSERT INTO Questions (CourseId, QuestionText, QuestionType, CreatedBy)
+        VALUES (@CourseId, @QuestionText, @QuestionType, @InstructorId);
+
+        SELECT SCOPE_IDENTITY() AS NewQuestionId;
+    END
+    ELSE
+    BEGIN
+        RAISERROR('Instructor not allowed to add question to this course.', 16, 1);
+    END
+END
