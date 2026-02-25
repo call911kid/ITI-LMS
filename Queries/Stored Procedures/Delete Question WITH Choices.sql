fad4e3f2197cc16376d794bce456ff1f168916e6
amr sobhy <am2873328@gmail.com>
2026-02-25 14:30:31 +0200

    Update Delete Question WITH Choices.sql

--- Queries/Stored Procedures/Delete Question WITH Choices.sql
+++ Queries/Stored Procedures/Delete Question WITH Choices.sql
@@ -1,23 +1,24 @@
 CREATE PROCEDURE USP_DeleteQuestion
-    @InstructorId INT,
     @QuestionId INT
 AS
 BEGIN
+    DECLARE @InstructorId INT = dbo.GetCurrentUserID();
+
     IF EXISTS (
         SELECT 1 
-        FROM Question q
-        JOIN Class c ON q.CourseId = c.CourseId
+        FROM Questions q
+        JOIN Courses c ON q.CourseId = c.CourseId
         WHERE q.QuestionId = @QuestionId AND c.InstructorId = @InstructorId
     )
     BEGIN
         DELETE FROM Choice
         WHERE QuestionId = @QuestionId;
 
-        DELETE FROM Question
+        DELETE FROM Questions
         WHERE QuestionId = @QuestionId;
     END
     ELSE
     BEGIN
         RAISERROR('Instructor not allowed to delete this question.', 16, 1);
     END
-END
+END;
