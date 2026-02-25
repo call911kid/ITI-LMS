b82f663c63883f99b795b98c676911f12ef9ae88
amr sobhy <am2873328@gmail.com>
2026-02-25 16:38:34 +0200

    Update Set Correct Answer.sql

--- Queries/Stored Procedures/Set Correct Answer.sql
+++ Queries/Stored Procedures/Set Correct Answer.sql
@@ -1,20 +1,28 @@
 CREATE PROCEDURE USP_SetCorrectAnswer
     @QuestionId INT,
-    @ChoiceId INT --  True/False
+    @ChoiceId INT
 AS
 BEGIN
     DECLARE @InstructorId INT = dbo.GetCurrentUserID();
 
     IF EXISTS (
         SELECT 1
-        FROM Questions q
-        JOIN Courses c ON q.CourseId = c.CourseId
+        FROM Question q
+        JOIN Course c ON q.CourseId = c.CourseId
+        JOIN Class cl ON cl.CourseId = c.CourseId
         WHERE q.QuestionId = @QuestionId
-          AND c.InstructorId = @InstructorId
+          AND cl.InstructorId = @InstructorId
     )
     BEGIN
-        UPDATE Questions
-        SET CorrectChoiceId = @ChoiceId
+        DECLARE @CorrectAnswer NVARCHAR(MAX);
+
+        SELECT @CorrectAnswer = ChoiceText
+        FROM Choice
+        WHERE ChoiceId = @ChoiceId
+          AND QuestionId = @QuestionId;
+
+        UPDATE Question
+        SET CorrectAnswer = @CorrectAnswer
         WHERE QuestionId = @QuestionId;
     END
     ELSE
