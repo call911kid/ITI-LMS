4fee44690995645de75a4f7f80526ee7b096f283
Reem Abdelkader <96788888+ReemAbdelkader@users.noreply.github.com>
2026-02-25 20:31:09 +0200

    seeding data into Intake table

---
+++ Data Seeding/Intake.sql
@@ -0,0 +1,14 @@
+-- Queries for Intake Table
+-- Firstly insert the data into table intake
+
+INSERT INTO [dbo].[Intake] (IntakeName , IntakeYear)
+VALUES 
+('Intake 39' , 2019),
+('Intake 40' , 2020),
+('Intake 41' , 2021),
+('Intake 42' , 2022),
+('Intake 43' , 2023),
+('Intake 44' , 2024),
+('Intake 45' , 2025),
+('Intake 46' , 2026)
+
