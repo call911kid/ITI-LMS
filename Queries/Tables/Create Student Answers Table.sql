3948c3b72eb527f46b29eef9159ae1fa12d0ccba
Mustafa Hassan <mustafawork911@gmail.com>
2026-03-02 03:22:54 +0200

    update Create StudentAnswers table to match FIX #3

--- Queries/Tables/Create Student Answers Table.sql
+++ Queries/Tables/Create Student Answers Table.sql
@@ -1,14 +1,14 @@
 CREATE TABLE [StudentAnswer](
 	[StudentAnswerId] INT IDENTITY(1,1),
 	[StudentExamId] INT NOT NULL,
-	[QuestionId] INT NOT NULL,
+	[ExamQuestionId] INT NOT NULL,
 	[AnswerText] NVARCHAR(MAX),
 	[MarksObtained] INT DEFAULT 0,
 
 	CONSTRAINT PK_StudentAnswer PRIMARY KEY([StudentAnswerId]),
 
-	CONSTRAINT FK_StudentAnswer_StudentExam FOREIGN KEY([StudentExamId]) REFERENCES [StudentExam]([StudentExamId]),
-	CONSTRAINT FK_StudentAnswer_Question FOREIGN KEY([QuestionId]) REFERENCES [Question]([QuestionId]),
+	CONSTRAINT FK_StudentAnswer_StudentExam FOREIGN KEY([StudentExamId]) REFERENCES [dbo].[StudentExam]([StudentExamId]),
+	CONSTRAINT FK_StudentAnswer_ExamQuestion FOREIGN KEY([ExamQuestionId]) REFERENCES [dbo].[ExamQuestion]([ExamQuestionId]),
 
-	CONSTRAINT Unique_StudentExam_Question UNIQUE([StudentExamId], [QuestionId]) -- prevent student from submitting an answer to the same question
+	CONSTRAINT Unique_StudentExam_ExamQuestion UNIQUE([StudentExamId], [ExamQuestionId]) -- prevent student from submitting an answer to the same exam question twice
 )
