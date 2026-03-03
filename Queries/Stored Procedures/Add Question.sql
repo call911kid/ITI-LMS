e3702e4cb6735202139896f07a82c1e9f1b60d1a
Reem Abdelkader <reemabdelkader97@gmail.com>
2026-03-04 01:56:48 +0200

    Update USP_AddQuestion to include CourseId parameter

--- Queries/Stored Procedures/Add Question.sql
+++ Queries/Stored Procedures/Add Question.sql
@@ -3,15 +3,18 @@
 CREATE PROCEDURE USP_AddQuestion
 	@QuestionBody NVARCHAR(200),
 	@QuestionType NVARCHAR(50),
-	@CorrectAnswer NVARCHAR(200)
+	@CorrectAnswer NVARCHAR(200),
+	@CourseId INT
 AS
 BEGIN
     BEGIN TRY
-        INSERT INTO Question (QuestionBody, QuestionType, CorrectAnswer)
-        VALUES (@QuestionBody, @QuestionType, @CorrectAnswer);
+        INSERT INTO Question (QuestionBody, QuestionType, CorrectAnswer , CourseId)
+        VALUES (@QuestionBody, @QuestionType, @CorrectAnswer , @CourseId);
     END TRY
     BEGIN CATCH
         EXEC [dbo].[USP_LogIntoAudit];
         THROW
     END CATCH
 END
+
+
